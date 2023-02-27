672aa3f3d1511a637e39db5d79210257ace7b334
Syifa Ulyanida <syifaulya0610@gmail.com>
2023-02-27 22:14:27 +0700

    initialize

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <p>Ini Konten</p>
+      <Footer />
+    </div>
+  );
+}
